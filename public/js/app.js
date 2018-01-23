0f561037d3dba0fc734418552c876d6b2cc9565d
= <eandrusjaks@gmail.com>
2018-01-23 21:45:09 +0200

    app.js update

--- public/js/app.js
+++ public/js/app.js
@@ -41555,7 +41555,7 @@ for (var i = 0; i < accordions.length; i++) {
 /* 50 */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: ModuleBuildError: Module build failed: \r\nundefined\r\n      ^\r\n      Undefined variable: \"$cyan\".\r\n      in C:\\Users\\Elvijs Andrusjaks\\Projects\\linecode\\linecode\\node_modules\\bulma\\sass\\utilities\\derived-variables.sass (line 3, column 8)\n    at runLoaders (C:\\Users\\Elvijs Andrusjaks\\Projects\\linecode\\linecode\\node_modules\\webpack\\lib\\NormalModule.js:195:19)\n    at C:\\Users\\Elvijs Andrusjaks\\Projects\\linecode\\linecode\\node_modules\\loader-runner\\lib\\LoaderRunner.js:364:11\n    at C:\\Users\\Elvijs Andrusjaks\\Projects\\linecode\\linecode\\node_modules\\loader-runner\\lib\\LoaderRunner.js:230:18\n    at context.callback (C:\\Users\\Elvijs Andrusjaks\\Projects\\linecode\\linecode\\node_modules\\loader-runner\\lib\\LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (C:\\Users\\Elvijs Andrusjaks\\Projects\\linecode\\linecode\\node_modules\\sass-loader\\lib\\loader.js:55:13)\n    at Object.<anonymous> (C:\\Users\\Elvijs Andrusjaks\\Projects\\linecode\\linecode\\node_modules\\async\\dist\\async.js:2257:31)\n    at Object.callback (C:\\Users\\Elvijs Andrusjaks\\Projects\\linecode\\linecode\\node_modules\\async\\dist\\async.js:958:16)\n    at options.error (C:\\Users\\Elvijs Andrusjaks\\Projects\\linecode\\linecode\\node_modules\\node-sass\\lib\\index.js:294:32)");
+// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
